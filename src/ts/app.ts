5fc1b2d9b3de30f61e926984fb123d58101193e7
xiaomo <83387856@qq.com>
2016-06-23 17:28:21 +0800

    点击事件

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -4,21 +4,22 @@
 import {Component} from '@angular/core'; 
 import { bootstrap } from '@angular/platform-browser-dynamic';
 
-import {BasicFormComponent} from './forms/basic-form';
-import {BasicComponent} from './basic/basic';
-import {NgForComponent} from './ngFor/NgFor'
-
+import {BasicFormComponent} from './forms/BasicForm';
+import {BasicComponent} from './basic/Basic';
+import {NgForComponent} from './ngFor/NgFor';
+import {ClickEventComponent} from './clickEvent/ClickEvent';
 require('../images/favicon.ico');
-require('./app.scss');
 
 @Component({
     selector: 'my-app',
+    styles:[require('./App.scss')],
     directives: [
         BasicFormComponent,
         BasicComponent,
-        NgForComponent
+        NgForComponent,
+        ClickEventComponent
     ],
-    template: require('./app.html')
+    template: require('./App.html')
 })
 class MyApp {
 }
--- src/ts/basic/basic.ts
+++ src/ts/basic/basic.ts
@@ -2,8 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'basic',
-    styles:[require('./basic.scss')],
-    template: require('./basic.html')
+    styles:[require('./Basic.scss')],
+    template: require('./Basic.html')
 })
 
 export class BasicComponent {
